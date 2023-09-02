335b6ae9f3c75066e9a281f9d314903a1011d14a
glody007 <glodymbutwile@gmail.com>
2023-09-02 18:33:15 +0200

    feat: Home page.

---
+++ config/mangas.ts
@@ -0,0 +1,62 @@
+export const listMangas = [
+	{
+        title:"One Piece",
+        author:"Eiichiro Oda",
+		href:"/one-piece",
+        src:"https://jw-webmagazine.com/wp-content/uploads/2020/05/One-Piece.jpg"
+	},
+	{
+        title:"Naruto",
+        author:"Masashi Kishimoto",
+		href:"/naruto",
+        src:"https://jw-webmagazine.com/wp-content/uploads/2020/05/Naruto.jpg"
+	},
+	{
+        title:"Kimetsu no Yaiba",
+        author:"Koyoharu Gotoge",
+		href:"demon-slayer",
+        src:"https://jw-webmagazine.com/wp-content/uploads/2020/05/Demon-Slayer.jpg"
+	},
+	{
+        title:"Hunter x Hunter",
+        author:"Yoshihiro Togashi",
+		href:"/hunterxhunter",
+        src:"https://jw-webmagazine.com/wp-content/uploads/2020/05/Hunter-x-Hunter.jpg"
+	},
+	{
+        title:"Fullmetal Alchemist",
+        author:"Hiromu Arakawa",
+		href:"/fullmetal",
+        src:"https://jw-webmagazine.com/wp-content/uploads/2020/05/Fullmetal-Alchemist.jpg"
+	},
+	{
+        title:"Attack on Titan",
+        author:"Hajime Isayama",
+		href:"/attack-on-titan",
+        src:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQAQkM3ZMpsMGl_sDLa83YAwHRxZpU6wd2PhIhJ89rpFl2saN76"
+	},
+	{
+        title:"Death Note",
+        author:"Tsugumi Ohba / Takeshi Obata",
+		href:"/death-note",
+        src:"https://jw-webmagazine.com/wp-content/uploads/2020/05/Death-Note.jpg"
+	},
+	{
+        title:"My Hero Academia",
+        author:"Kohei Horikoshi",
+		href:"/my-hero-academia",
+        src:"https://jw-webmagazine.com/wp-content/uploads/2020/05/My-Hero-Academia.jpg"
+	},
+	{
+        title:"Case Closed",
+        author:"Gosho Aoyama",
+		href:"/case-closed",
+        src:"https://jw-webmagazine.com/wp-content/uploads/2020/05/Case-Closed.jpg"
+	},
+	{
+        title:"Bleach",
+        author:"Tite Kubo",
+		href:"/bleach",
+        src:"https://jw-webmagazine.com/wp-content/uploads/2020/05/Bleach.jpg"
+	}
+]
